a2fbcfb39085dc5175c232af4aa107e97d577190
yash030706 <78734494+yash030706@users.noreply.github.com>
2021-03-27 10:38:45 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,30 @@
+class bob{
+	constructor(x,y,r){
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+		}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display(){
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
